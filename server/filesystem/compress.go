f3fed4bd81f90ce28ff010beb5956b43a8d7216e
Linux123123 <linas.a@sparkedhost.com>
2024-12-28 17:04:59 +0200

    Fix typo and rephrase

--- server/filesystem/compress.go
+++ server/filesystem/compress.go
@@ -85,7 +85,8 @@ func (fs *Filesystem) archiverFileSystem(ctx context.Context, p string) (iofs.FS
 			// zip.Reader doesn't suffer from issue #330 and #310 according to local test (but they should be fixed anyway)
 			return zip.NewReader(f, info.Size())
 		case archives.Extraction:
-			// Returning pointer in neccessary, as only on pointer there is implementation of ReadDir method
+			// Returning a pointer is necessary, as the ReadDir method is only implemented for pointers
+			// and it is used for walking the archive fs.
 			return &archives.ArchiveFS{Stream: io.NewSectionReader(f, 0, info.Size()), Format: ff, Context: ctx}, nil
 		case archives.Compression:
 			return archiverext.FileFS{File: f, Compression: ff}, nil
