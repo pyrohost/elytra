229c8afe9c871885007b5aec67fe5636f37b8395
Linux123123 <linas.a@sparkedhost.com>
2025-01-04 11:15:15 +0200

    Add a simple test for regression
    Co-authored-by: Arnaud Lier <arnaud@lier.link>

--- server/filesystem/compress_test.go
+++ server/filesystem/compress_test.go
@@ -52,3 +52,38 @@ func TestFilesystem_DecompressFile(t *testing.T) {
 		})
 	})
 }
+
+func TestFilesystem_SpaceAvailableForDecompression(t *testing.T) {
+    g := Goblin(t)
+    fs, rfs := NewFs()
+
+    g.Describe("SpaceAvailableForDecompression", func() {
+        for _, ext := range []string{"zip", "rar", "tar", "tar.gz"} {
+            g.It("should succeed when enough space is available for decompression of a "+ext, func() {
+                fs.SetDiskLimit(2048)
+
+                // copy the file to the new FS
+                c, err := os.ReadFile("./testdata/test." + ext)
+                g.Assert(err).IsNil()
+                err = rfs.CreateServerFile("./test."+ext, c)
+                g.Assert(err).IsNil()
+
+                err = fs.SpaceAvailableForDecompression(context.Background(), "./", "test."+ext)
+                g.Assert(err).IsNil()
+            })
+
+            g.It("should fail when not enough space is available for decompression of a "+ext, func() {
+                fs.SetDiskLimit(12)
+
+                // copy the file to the new FS
+                c, err := os.ReadFile("./testdata/test_13b." + ext)
+                g.Assert(err).IsNil()
+                err = rfs.CreateServerFile("./test_13b."+ext, c)
+                g.Assert(err).IsNil()
+
+                err = fs.SpaceAvailableForDecompression(context.Background(), "./", "test_13b."+ext)
+                g.Assert(err.Error()).Equal("filesystem: not enough disk space")
+            })
+        }
+    })
+}
