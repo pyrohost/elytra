3d7d474257792188082707483af04609202d258f
Elizabeth <me@checksum.space>
2025-09-20 11:39:04 -0500

    chore: :skull: claude did not need to do all that

---
+++ src/cmd/elytra/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"math/rand"
+
+	"time"
+
+	"github.com/pyrohost/elytra/src/cmd"
+)
+
+func main() {
+
+	// Since we make use of the math/rand package in the code, especially for generating
+
+	// non-cryptographically secure random strings we need to seed the RNG. Just make use
+
+	// of the current time for this.
+
+	rand.Seed(time.Now().UnixNano())
+
+	// Execute the main binary code.
+
+	cmd.Execute()
+
+}
